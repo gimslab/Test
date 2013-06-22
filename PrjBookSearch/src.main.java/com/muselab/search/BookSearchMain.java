7f22ef0073383ba19567c8f60569e7702ea9cd12
gim <gimslab.com@gmail.com>
2013-06-22 10:27:48 +0900

    BookSearchServiceImpl을 직접 참조하면 interface를 만든 의미가 없지 않은가? 스프링을 사용하여 BookSearchMain.java에서 BookSearchServiceImpl을 직접 참조하지 않도록 하는게 좋을듯

--- PrjBookSearch/src.main.java/com/muselab/search/BookSearchMain.java
+++ PrjBookSearch/src.main.java/com/muselab/search/BookSearchMain.java
@@ -3,7 +3,7 @@ package com.muselab.search;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-//import com.muselab.search.service.BookSearchService;
+import com.muselab.search.service.BookSearchService;
 import com.muselab.search.service.BookSearchServiceImpl;
 
 public class BookSearchMain {
@@ -13,11 +13,12 @@ public class BookSearchMain {
 		ArrayList<HashMap<String, String>> booklist = new ArrayList<HashMap<String, String>>();
 		String srh = "ㅇㄴ";
 		
-		BookSearchServiceImpl bookSearchServiceImpl = new BookSearchServiceImpl();
+//		BookSearchServiceImpl bookSearchServiceImpl = new BookSearchServiceImpl();
+		BookSearchService bookSearchService = new BookSearchServiceImpl();
 		
 		try {
 			System.out.println("--------" );
-			booklist = bookSearchServiceImpl.getSearchBookList(srh);
+			booklist = bookSearchService.getSearchBookList(srh);
 			
 			for(int i=0; i<= booklist.size(); i++) {
 				
